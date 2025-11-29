3bbfb94db733fde09fa58d311edee6c7f16028d4
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-29 20:28:01 +0530

    if_else

---
+++ Lec-04/if_else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int  main(){
+int package;
+ cin>>package;
+ if(package>12){
+    cout<<"Good Package";
+ }else{
+    cout<<"Not Good";
+ }
+ return 0;
+}
+ 
