b1e754de9ead8acd65b123bb2af6a8d892bccfb9
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-30 19:20:24 +0530

    powervalue

--- Lec-05/Powerfoanyvalue.cpp
+++ Lec-05/Powerfoanyvalue.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num,n;
-    cout<<"Enter num value:";
-    cin>>num;
-    int pow ;
-    cout<<"Enter pow value:";
-    cin>>pow;
-    n= num;
-    for(int i=1;i<pow;i++){
-    n = num*n;
+    int base;
+    cout<<"Enter your base value:";
+    cin>>base;
+    int power;
+    cout<<"Enter your Power value:";
+    cin>>power;
+    int result =1;
+    for(int i=0;i<power;i++){
+        result = result*base;
     }
-    cout<<n;
+    cout<<base<<"^"<<power<<"="<<result<<endl;
     return 0;
 }
