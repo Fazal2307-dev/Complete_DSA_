ff3fc36abae58e5b03a72a7da856b74b950fa860
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-01 22:04:42 +0530

    the dice problem

---
+++ Practice_Problem/1_Basic/7_the_dice_problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+int  main(){
+    int n;
+    cout<<"Enter vqaalue of n:";
+    cin>>n;
+    int ans;
+    if(n==1)
+    cout<<"6";
+    else if(n==2)
+    cout<<"5";
+    else if(n==3)
+    cout<<"4";
+    else if(n==4)
+    cout<<"5";
+    else
+    cout<<"6";
+    return 0;
+}
