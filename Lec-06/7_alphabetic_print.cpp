7d86cc075606ed53957ef69dec4bda9edd34225b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-02 19:04:10 +0530

    Pattern_PRoblem

---
+++ Lec-06/7_alphabetic_print.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n:";
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        char name = 'a'+(row-1);
+        for(int col=1;col<=n;col++){
+            cout<<name<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
