6e245427303f21eff9b27871462f69119ce0ed3e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-03 18:44:50 +0530

    Pattern

---
+++ Lec-07/8_right_trangle_col.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n:";
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int col=n;col>=n-row+1;col--){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
