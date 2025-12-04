bf4c606ffdde1ad3b50cd5544a663a40a080fbcf
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-04 19:01:34 +0530

    pattern

---
+++ Lec-09/5_print_pattern_reverse_piramide.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your value of n:";
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            cout<<"* ";
+        }
+        for(int col=1;col<=(2*n-2*row);col++){
+            cout<<"  ";
+        }
+        for(int col=1;col<=row;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+     }
+     //reverse
+     for(int row=n-1;row>=1;row--){
+        for(int col=1;col<=row;col++){
+            cout<<"* ";
+        }
+        for(int col=1;col<=(2*n-2*row);col++){
+            cout<<"  ";
+        }
+        for(int col=1;col<=row;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+     }
+     return 0;
+}
