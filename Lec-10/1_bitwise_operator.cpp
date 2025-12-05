9baf9dd27c5e8ebdd63ed64a93ebb24041e4af22
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-05 22:18:09 +0530

    problem

--- Lec-10/1_bitwise_operator.cpp
+++ Lec-10/1_bitwise_operator.cpp
@@ -5,7 +5,11 @@ int main()
     int a =2,b=3;
     //& operator
     int ans = a&b;
-    cout<<ans;
+    cout<<ans<<endl;
+    int ore = a|b;
+    cout<<ore<<endl;
+    int axor = a^b;
+    cout<<axor<<endl;
     
     return 0;
 }
