5d83421f98808f30bf54bad03a23bce1e7399331
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-06 21:47:54 +0530

    problem

---
+++ Lec-12/6_problem.cpp
@@ -0,0 +1,12 @@
+/*What will be the result below according to the precedence table.
+ 6<<2-4*8/2
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int ans =  6<<2-4*8/2;
+    cout<<ans<<endl;
+    return 0;
+
+}
