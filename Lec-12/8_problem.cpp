5a7b22cb5983746db8e64610157c8c86fe1f2f71
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-06 21:48:17 +0530

    problem

---
+++ Lec-12/8_problem.cpp
@@ -0,0 +1,12 @@
+/*What will be the result below according to the precedence table.
+ 14-8+92>>2+70
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int ans= 14-8+92>>2+70;
+    cout<<ans<<endl;
+    return 0;
+
+}
