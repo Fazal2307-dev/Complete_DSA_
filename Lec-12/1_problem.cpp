8e717a66988b7e7bdaf5c7cedb7b6b029aed7d13
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-06 21:47:01 +0530

    problem

---
+++ Lec-12/1_problem.cpp
@@ -0,0 +1,18 @@
+/*1: Temperature Range: Write a program that checks if a given temperature is suitable for swimming. 
+If the temperature is between 70 and 90 (Excluded) degrees Fahrenheit print yes, else NO. 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+int temp;
+cout<<"Enter water temp:";
+cin>>temp;
+if(temp>=70 && temp<=90)
+cout<<"yes";
+else
+cout<<"NO";
+cout<<endl;
+return 0;
+
+}
