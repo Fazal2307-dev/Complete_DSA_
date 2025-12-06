7a4eea474d9512ee4718c02ecba8121a4b52a813
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-06 21:48:04 +0530

    problem

---
+++ Lec-12/7_problem.cpp
@@ -0,0 +1,12 @@
+/*What will be the result below according to the precedence table.
+ 5>4<3/2-8%4+5
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int ans =  5>4<3/2-8%4+5;
+    cout<<ans<<endl;
+    return 0;
+
+}
