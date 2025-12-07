5af67b161d04ad4603a51e12587385490931493f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-07 14:51:46 +0530

    problem

---
+++ Lec-12/11_problem.cpp
@@ -0,0 +1,17 @@
+//binary to decimal
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter valu eof num:";
+    cin>>num;
+    int rem,ans=0,mul=1;
+    while(num>0){
+        rem = num%10;
+        num/=10;
+        ans = rem*mul+ans;
+        mul*=2;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
