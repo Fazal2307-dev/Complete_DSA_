6eac3fc785ebd6c149372655bacaeb0f622a792c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-08 14:19:19 +0530

    problem and solution

---
+++ Lec-13/3_problem.cpp
@@ -0,0 +1,19 @@
+//reverse aa number
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int num;
+    cout<<"Enter you num:";
+    cin>>num;
+    int ans =0,rem;
+    while(num!=0){
+        rem = num %10;
+        num/=10;
+        // if(ans>INT_MAX/10 || ans<INT_MIN/10)
+        // return 0;
+        ans = ans*10 + rem;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
