bdde46425bf0e7834f9ed7a7b5af2cc6d75e6b27
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-08 14:19:06 +0530

    problem and solution

---
+++ Lec-13/2_problem.cpp
@@ -0,0 +1,17 @@
+//Leap year
+#include<iostream>
+using namespace std;
+int main(){
+    int year ;
+    cout<<"Enter value of your:";
+    cin>>year;
+    if(year%400 ==0){
+        cout<<"Leap Year";
+    }else if(year % 4==0 && year % 100 !=0){
+        cout<<"Leap Year";
+    }
+    else{
+        cout<<"Not Leap Year";
+    }
+    return 0;
+}
